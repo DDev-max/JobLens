80bae5e9810fa16433bb988e3f7dbfff07244cd5
Claus <tab88fn@gmail.com>
2025-04-03 17:17:01 -0600

    refactor: fetch is made using a 'while'. Fetch test

--- src/Utils/fetchData/fetchData.ts
+++ src/Utils/fetchData/fetchData.ts
@@ -6,40 +6,34 @@ interface FetchFnProps {
 }
 type ResponseTypes = 'json' | 'text'
 
-let attemptCounter = 0
-
 export async function fetchData<TFetchReturn>({ URL, responseType, retries }: FetchFnProps) {
   const validTypes: readonly [ResponseTypes, ResponseTypes] = ['json', 'text']
+  let attemptCounter = 0
 
-  try {
-    attemptCounter++
-
-    const response = await fetch(URL)
-
-    if (!validTypes.includes(responseType)) {
-      throw new ResponseTypeError(`"${responseType}" is an invalid response type.`)
+  while (attemptCounter < retries) {
+    try {
+      attemptCounter++
+      if (!validTypes.includes(responseType)) {
+        throw new ResponseTypeError(`"${responseType}" is an invalid response type.`)
+      }
+      const response = await fetch(URL)
+      if (!response.ok) {
+        throw new Error(`Fetch error: ${response.status}, ${response.statusText}`)
+      }
+      const format: TFetchReturn = await response[responseType]()
+      return format
+    } catch (error) {
+      /* eslint-disable no-console */
+      if (error instanceof Error) {
+        console.error(`Error: ${error.name}, ${error.message} \n URL: ${URL}`)
+        if (error instanceof ResponseTypeError || attemptCounter >= retries) {
+          if (attemptCounter >= retries) {
+            console.error(`Fetch has failed after ${retries} tries`)
+          }
+          throw error
+        }
+      }
     }
-
-    if (!response.ok) {
-      throw new Error(`Fetch error: ${response.status}, ${response.statusText}`)
-    }
-
-    const format: TFetchReturn = await response[responseType]()
-
-    return format
-  } catch (error) {
-    /* eslint-disable no-console */
-
-    if (error instanceof Error) {
-      console.error(`Error: ${error.name}, ${error.message} \n URL: ${URL}`)
-      if (error instanceof ResponseTypeError) return
-    }
-
-    if (attemptCounter < retries) {
-      console.log(`Retrying... Attempt ${attemptCounter + 1} of ${retries}`)
-      return await fetchData({ responseType, retries, URL })
-    }
-
-    console.error(`Fetch has failed after ${retries} tries`)
+    console.log(`Retrying... Attempt ${attemptCounter + 1} of ${retries}`)
   }
 }
